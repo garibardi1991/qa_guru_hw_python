231d552af21992029f096b38aab5ef499b207ab7
igor trubikhov <trubikhov.i@leads.su>
2024-06-12 18:20:28 +0300

    Исправил fixture и размеры

--- conftest.py
+++ conftest.py
@@ -1,10 +1,12 @@
 import pytest
-from selene import browser, have
+from selene import browser
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session', autouse=True)
 def browser_driver():
     browser.config.driver_name = 'firefox'
+    browser.config.window_height = 1920
+    browser.config.window_width = 1080
 
     yield
     browser.quit()
--- test_google.py
+++ test_google.py
@@ -2,18 +2,18 @@ import pytest
 from selene import browser, have
 
 
-@pytest.fixture
-def browser_size(browser_driver):
-    browser.driver.set_window_size(1920, 1080)
+@pytest.fixture(autouse=True)
+def browser_url(browser_driver):
+    browser.config.base_url = 'https://google.com'
 
 
-def test_search_goggle(browser_size):
-    browser.open('https://google.com')
+def test_search_goggle():
+    browser.open('')
     browser.element('[name=q]').type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_no_search_goggle(browser_size):
-    browser.open('https://google.com')
+def test_no_search_goggle():
+    browser.open('')
     browser.element('[name=q]').type('2353564tut6u77i').press_enter()
-    browser.element('#botstuff').should(have.text('ничего не найдено.'))
+    browser.element('#botstuff').should(have.text('По запросу 2353564tut6u77i ничего не найдено.'))
