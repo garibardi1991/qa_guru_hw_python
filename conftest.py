dff5b9c202794cacb3c7abd1aabbe20d7d71e305
igor trubikhov <trubikhov.i@leads.su>
2024-06-12 15:50:35 +0300

    add 2 test

--- conftest.py
+++ conftest.py
@@ -5,5 +5,9 @@ from selene import browser, have
 @pytest.fixture(scope='session')
 def browser_driver():
     browser.config.driver_name = 'firefox'
+
     yield
     browser.quit()
+
+
+
--- test_google.py
+++ test_google.py
@@ -1,13 +1,19 @@
+import pytest
 from selene import browser, have
 
 
-def test_search_goggle(browser_driver):
+@pytest.fixture
+def browser_size(browser_driver):
+    browser.driver.set_window_size(1920, 1080)
+
+
+def test_search_goggle(browser_size):
     browser.open('https://google.com')
     browser.element('[name=q]').type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_no_search_goggle(browser_driver):
+def test_no_search_goggle(browser_size):
     browser.open('https://google.com')
     browser.element('[name=q]').type('2353564tut6u77i').press_enter()
     browser.element('#botstuff').should(have.text('ничего не найдено.'))
