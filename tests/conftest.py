3ecb5633208a8423865bf7ed2837f0d09acf814b
igor trubikhov <trubikhov.i@leads.su>
2024-06-28 18:32:07 +0300

    Home work paython-7

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import zipfile
+import pytest
+from script_os import FILES_DIR, FOLDER_DIR
+
+
+@pytest.fixture(scope="session", autouse=True)
+def files_archive():
+    if not os.path.exists(FOLDER_DIR):
+        os.mkdir(FOLDER_DIR)
+        with zipfile.ZipFile(os.path.join(FOLDER_DIR, "my_archive.zip"), 'w') as zf:
+            for file in os.listdir(FILES_DIR):
+                zf.write(os.path.join(FILES_DIR, file), file)
+
+    yield
+    shutil.rmtree(FOLDER_DIR)
---
+++ tests/test_files.py
@@ -0,0 +1,42 @@
+import csv
+import zipfile
+from openpyxl import load_workbook
+from pypdf import PdfReader
+from script_os import ZIP_DIR
+
+
+def test_pdf_file():
+    with zipfile.ZipFile(ZIP_DIR, 'r') as zip_file:
+        with zip_file.open("PDF_Converter_Pro_Quick_Reference_Guide.RU.pdf") as pdf_file:
+            reader = PdfReader(pdf_file)
+            page = reader.pages[2]
+            text = page.extract_text()
+            assert '3Почему  популярны  файлы  PDF?\n' in text
+
+
+def test_xlsx_file():
+    with zipfile.ZipFile(ZIP_DIR, 'r') as zip_file:
+        with zip_file.open("test_file.xlsx") as excel_file:
+            wb = load_workbook(filename=excel_file)
+            sheet = wb.active
+            cell_value = sheet.cell(row=1, column=3).value
+            print (f"Содержимое ячейки в строке {1} и столбце {3}: {cell_value}")
+            name = 'Дата создания'
+            assert name in cell_value, f"Название столбца: {name} присутствует в файле"
+
+
+def test_csv():
+    with zipfile.ZipFile(ZIP_DIR, 'r') as zip_file:
+        with zip_file.open("import_empl.csv") as csv_file:
+            content = csv_file.read().decode(
+                'utf-8-sig')
+            csvreader = list(csv.reader(content.splitlines()))
+            second_row = csvreader[1]  # получаем вторую строку
+            result_list = second_row[0].split(',')
+            namePostback = "тетапвар"
+            IDPostback = 'CS4164U'
+
+            assert result_list[2] == namePostback, f"Название постбэка: {namePostback
+            } присутствует в таблице {result_list}"  # проверка значения элемента в первом столбце второй строки
+            assert result_list[3] == IDPostback, f"ID по: {IDPostback
+            } присутствует в таблице {IDPostback}"  # проверка значения элемента во втором столбце второй строки
