4b2532cf31a66f9821a4bbb4aa0ec9e8b9e0efff
garibardi1991 <garibardi@mail.ru>
2024-07-12 15:20:31 +0300

    add allure

--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -1,24 +1,32 @@
+import allure
 from pages.form_page import RegistrationForm
 
 
+@allure.title("Successful fill form")
 def test_student_registration_form():
-    registration_form = RegistrationForm()
-    registration_form.open()
-    registration_form.type_first_name('Igor')
-    registration_form.type_last_name('Trubikhov')
-    registration_form.type_email('garibardi@mail.ru')
-    registration_form.type_birthday()
-    registration_form.click_gender()
-    registration_form.type_phone('89523811047')
-    registration_form.type_subjects('Maths')
-    registration_form.click_hobbies()
-    registration_form.select_picture('istockphoto-1443562748-612x612.jpg')
-    registration_form.type_address('2-y Karavannay 15-4')
-    registration_form.type_state('Haryana')
-    registration_form.type_city("Karnal")
-    registration_form.press_submit()
-    registration_form.should_text('Thanks for submitting the form')
-    registration_form.should_exact_text(
+
+    with allure.step("Open registrations form"):
+        registration_form = RegistrationForm()
+        registration_form.open()
+
+    with allure.step("Fill form"):
+        registration_form.type_first_name('Igor')
+        registration_form.type_last_name('Trubikhov')
+        registration_form.type_email('garibardi@mail.ru')
+        registration_form.type_birthday()
+        registration_form.click_gender()
+        registration_form.type_phone('89523811047')
+        registration_form.type_subjects('Maths')
+        registration_form.click_hobbies()
+        registration_form.select_picture('istockphoto-1443562748-612x612.jpg')
+        registration_form.type_address('2-y Karavannay 15-4')
+        registration_form.type_state('Haryana')
+        registration_form.type_city("Karnal")
+        registration_form.press_submit()
+
+    with allure.step("Check form results"):
+        registration_form.should_text('Thanks for submitting the form')
+        registration_form.should_exact_text(
             'Igor Trubikhov',
             'garibardi@mail.ru',
             'Male',
