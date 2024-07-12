b8ac58c92cf994b227e11e5dc4d53c9878a8e284
garibardi1991 <garibardi@mail.ru>
2024-07-12 17:23:16 +0300

    add allure

--- tests/test_demo_qa.py
+++ tests/test_demo_qa.py
@@ -9,6 +9,7 @@ from allure_commons.types import Severity
 @allure.feature("Автотест с Allure DemoQA")
 @allure.story("Тестирование формы регистрации DEMOQA")
 @allure.link("https://demoqa.com/", name="Testing")
+@allure.title("Тестирование заполнения формы регистриации")
 def test_student_registration_form(setup_browser):
 
     with allure.step("Открытие регистрационной формы"):
