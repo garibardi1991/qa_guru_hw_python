0ae1f34487f0bcbf64be20b6348d8f4f70b4b8b2
igor trubikhov <trubikhov.i@leads.su>
2024-08-04 21:48:31 +0300

    Home work pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,47 @@
 import pytest
 from selene import browser
-from selenium import webdriver
 
 
-@pytest.fixture(scope='session', autouse=True)
-def browser_driver():
-    # browser.config.driver_name = 'firefox'
-    browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless=new')
-    browser.config.driver_options = driver_options
-    driver_options.page_load_strategy = 'eager'
-    browser.config.window_height = 1200
-    browser.config.window_width = 1920
+@pytest.fixture(scope='function', params=[(1280, 720), (1920, 1080), (2560, 1440)],
+                ids=['WEB_1280', 'WEB_1920', 'WEB_2560'])
+def browser_management_desktop(request):
+    browser.config.base_url = 'https://github.com'
+
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
 
     yield
+
     browser.quit()
 
 
+@pytest.fixture(scope='function', params=[(414, 896), (360, 740), (480, 854)],
+                ids=['Mobile_414', 'Mobile_360', 'Mobile_480'])
+def browser_management_mobile(request):
+    browser.config.base_url = 'https://github.com'
 
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', params=[(1280, 720), (1920, 1080), (414, 896)],
+                ids=['WEB_1280', 'WEB_1920', 'Mobile_414'])
+def browser_management(request):
+    browser.config.base_url = 'https://github.com'
+
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
+
+    if width >= 800:
+        yield 'desktop'
+    else:
+        yield 'mobile'
+
+    browser.quit()
