92b6e5ea16879010c959915291ff3bafffd39840
igor trubikhov <trubikhov.i@leads.su>
2024-08-20 17:02:56 +0300

    Home work mobile_tests

--- tests/test_mobile_app.py
+++ tests/test_mobile_app.py
@@ -1,16 +1,10 @@
-import allure
 from allure_commons._allure import step
 from appium.webdriver.common.appiumby import AppiumBy
-from selene import browser, have, be
+from selene import browser, have
 
 
-@allure.tag("mobile")
-@allure.label("owner", "Igor Trubikhov")
-@allure.feature("Википедия_примеры тесов для мобильных устройств")
-@allure.story("Поиск на сайте")
-def test_search_wikipedia():
+def test_search():
     with step('Type search'):
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
 
@@ -20,15 +14,15 @@ def test_search_wikipedia():
         results.first.should(have.text('Appium'))
 
 
-@allure.tag("mobile")
-@allure.label("owner", "Igor Trubikhov")
-@allure.feature("Википедия_примеры тесов для мобильных устройств")
-@allure.story("Открытие результата поиска")
-def test_open_search_result_wikipedia(mobile_management):
-    with step('Type search'):
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click()
+def test_open_first_article():
+    with step('Type search "Java"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('browserstack')
-    with step('Open search result'):
-        browser.all((AppiumBy.ID, "org.wikipedia.alpha:id/page_list_item_title")).first.click()
-        browser.all((AppiumBy.XPATH, '//android.webkit.WebView[@text="BrowserStack"]')).should(be.present)
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Java')
+
+    with step('Verify content found.'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Java'))
+
+    with step('Click the first article.'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
