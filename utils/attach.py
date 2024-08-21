7bd0199c43dd69846bdc03237544aafea62aeff6
igor trubikhov <trubikhov.i@leads.su>
2024-08-21 09:40:14 +0300

    Home work mobile_tests

--- utils/attach.py
+++ utils/attach.py
@@ -5,20 +5,20 @@ from allure_commons.types import AttachmentType
 
 def add_screenshot(browser):
     png = browser.driver.get_screenshot_as_png()
-    allure.attach(body = png, name = 'screenshot', attachment_type = AttachmentType.PNG, extension = '.png')
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
 
 def add_xml(browser):
     xml_dump = browser.driver.page_source
-    allure.attach(body = xml_dump,
-                  name = 'XML screen',
-                  attachment_type = AttachmentType.XML)
+    allure.attach(body=xml_dump,
+                  name='XML screen',
+                  attachment_type=AttachmentType.XML)
 
 
 def add_video(session_id, login, access_key):
     browserstack_session = requests.get(
-        url = f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
-        auth = (login, access_key)
+        url=f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
+        auth=(login, access_key)
     ).json()
     video_url = browserstack_session['automation_session']['video_url']
 
@@ -28,6 +28,6 @@ def add_video(session_id, login, access_key):
         f'<source src="{video_url}" type="video/mp4">'
         '</video>'
         '</body></html>',
-        name = 'video recording',
-        attachment_type = AttachmentType.HTML,
+        name='video recording',
+        attachment_type=AttachmentType.HTML,
     )
